f722820cece3368825349480f1035d98a98bb0ac
Stéphane Derosiaux <derste@gmail.com>
2019-05-18 00:31:39 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,25 @@
+name := "heatzy"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+val http4sVersion= "0.20.1"
+libraryDependencies += "co.fs2" %% "fs2-core" % "1.0.4"
+libraryDependencies += "co.fs2" %% "fs2-io" % "1.0.4"
+libraryDependencies += "com.lihaoyi" %% "requests" % "0.1.8"
+libraryDependencies += "org.http4s" %% "http4s-core" % http4sVersion
+libraryDependencies += "org.http4s" %% "http4s-circe" % http4sVersion
+libraryDependencies += "org.http4s" %% "http4s-client" % http4sVersion
+libraryDependencies += "org.http4s" %% "http4s-dsl" % http4sVersion
+libraryDependencies += "org.http4s" %% "http4s-blaze-client" % http4sVersion
+libraryDependencies += "org.http4s" %% "http4s-prometheus-metrics" % http4sVersion
+libraryDependencies += "io.circe" %% "circe-generic" % "0.11.1"
+libraryDependencies += "io.circe" %% "circe-literal" % "0.11.1"
+libraryDependencies += "com.typesafe" % "config" % "1.3.4"
+
+scalacOptions ++= Seq("-Ypartial-unification")
+
+addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.0")
+addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.10.0")
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
---
+++ src/main/scala/com/sderosiaux/heatzy/Heatzy.scala
@@ -0,0 +1,90 @@
+package com.sderosiaux.heatzy
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+import com.typesafe.config.ConfigFactory
+import io.circe.generic.auto._
+import org.http4s._
+import org.http4s.circe.CirceEntityCodec._
+import org.http4s.client.Client
+import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.client.middleware.{RequestLogger, ResponseLogger}
+import org.http4s.headers.{Accept, `Content-Type`}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+case class LoginRequest(username: String, password: String, lang: String = "en")
+
+case class LoginResponse(token: String, uid: String, expire_at: Long)
+
+case class Device(protoc: Int, is_disabled: Boolean, dev_alias: String)
+
+case class BindingsResponse(devices: List[Device])
+
+case class Heatzy(url: String, appId: String)
+
+/*
+https://drive.google.com/drive/folders/0B9nVzuTl4YMOaXAzRnRhdXVma1k
+ */
+
+object Heatzy extends IOApp {
+  val config = ConfigFactory.load()
+  val heatzy = Heatzy(config.getString("heatzy.cloud.url"), config.getString("heatzy.app.id"))
+
+
+  //implicit val lrDecoder: EntityDecoder[IO, LoginResponse] = jsonOf[IO, LoginResponse]
+
+  val p: Option[String => IO[Unit]] = ((x: String) => IO(println(x))).some
+
+  def post[A, B](path: String, body: A)(implicit d: EntityDecoder[IO, B], d2: EntityEncoder[IO, A], client: Client[IO]): IO[B] = {
+    val res = Request[IO](
+      method = Method.POST,
+      uri = Uri.unsafeFromString(s"${heatzy.url}$path"),
+      headers = Headers.of(
+        Accept(MediaType.application.json),
+        `Content-Type`(MediaType.application.json),
+        Header("X-Gizwits-Application-Id", heatzy.appId)
+      )
+    ).withEntity(body)
+
+    client.fetchAs[B](res)
+  }
+
+  def get[A](path: String, token: String)(implicit client: Client[IO], d: EntityDecoder[IO, A]): IO[A] = {
+    val res = Request[IO](
+      method = Method.GET,
+      uri = Uri.unsafeFromString(s"${heatzy.url}$path"),
+      headers = Headers.of(
+        Accept(MediaType.application.json),
+        `Content-Type`(MediaType.application.json),
+        Header("X-Gizwits-Application-Id", heatzy.appId),
+        Header("X-Gizwits-User-token", token)
+      )
+    )
+
+    client.fetchAs[A](res)
+  }
+
+  def bindings(token: String)(implicit client: Client[IO]): IO[BindingsResponse] = {
+    get[BindingsResponse]("/bindings", token)
+  }
+
+  def login(username: String, password: String)(implicit client: Client[IO]): IO[LoginResponse] = {
+    val body = LoginRequest(username, password)
+    post[LoginRequest, LoginResponse]("/login", body)
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    BlazeClientBuilder[IO](global).resource.use { c =>
+      implicit val client: Client[IO] = ResponseLogger(logHeaders = true, logBody = true, logAction = p)(
+        RequestLogger(logHeaders = true, logBody = true, logAction = p)(c)
+      )
+
+      for {
+        l <- login("XXX@gmail.com", "XXX")
+        b <- bindings(l.token)
+        _ <- IO(println(b))
+      } yield ExitCode.Success
+    }
+  }
+}
